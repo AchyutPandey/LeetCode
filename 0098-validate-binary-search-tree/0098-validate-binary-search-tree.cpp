36bc39f0dc8bd236847ed2cdee700826e72241dd
AchyutPandey <121763772+AchyutPandey@users.noreply.github.com>
2024-07-15 15:31:02 +0530

    Time: 6 ms (79.53%), Space: 20.4 MB (10.13%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -6,15 +6,24 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
+    bool helper(TreeNode* root, long int l, long int h) {
+        if (root == nullptr)
+            return true;
+        if (root->val >= h || root->val <= l) {
+            return false;
+        }
+        return helper(root->left, l, root->val) &&
+               helper(root->right, root->val , h);
+        
+    }
     bool isValidBST(TreeNode* root) {
-        if(root==nullptr)return true;
-        if(root->right&&root->right->val<=root->val)return false;
-        if(root->left&&root->left->val>=root->val)return false;
-        return isValidBST(root->left) && isValidBST(root->right);
+        long long int l=-1000000000000,r=1000000000000;
+        return helper(root, l, r); 
     }
 };
