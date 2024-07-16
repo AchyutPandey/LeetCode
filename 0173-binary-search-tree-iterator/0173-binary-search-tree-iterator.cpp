47c3adc4e27ddcce2b2913145d9f37f0197a291f
AchyutPandey <121763772+AchyutPandey@users.noreply.github.com>
2024-07-16 18:00:21 +0530

    Time: 28 ms (10.45%), Space: 29.4 MB (6.96%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -13,30 +13,40 @@ class BSTIterator {
 public:
     vector<int> inorder;
     int ind;
-    int size;
-    void func(vector<int> &arr,TreeNode *root){
-        if(root->left!=nullptr){
-            func(arr,root->left);
+
+    // Helper function to perform in-order traversal
+    void func(vector<int> &arr, TreeNode *root) {
+        if (root->left != nullptr) {
+            func(arr, root->left);
         }
         arr.push_back(root->val);
-        if(root->right){
-            func(arr,root->right);
+        if (root->right != nullptr) {
+            func(arr, root->right);
         }
     }
+
+    // Constructor to initialize the BSTIterator
     BSTIterator(TreeNode* root) {
-        func(inorder,root);
-        ind=-1;
-        size=inorder.size();
+        func(inorder, root);
+        ind = -1; // Initialize index to -1
+        cout << "Inorder Traversal: ";
+        for (int val : inorder) {
+            cout << val << " ";
+        }
+        cout << endl;
     }
-    
+
+    // Return the next element in the in-order traversal
     int next() {
-        ind++;
-        return inorder[ind];
+        ind++; // Move to the next index
+        cout << "Next index: " << ind << ", Value: " << inorder[ind] << endl;
+        return inorder[ind]; // Return the current element
     }
-    
+
+    // Check if there is a next element
     bool hasNext() {
-        if(ind>=size-1)return false;
-        return true;
+        cout << "Current index: " << ind << ", Inorder size: " << inorder.size() << endl;
+        return ind+1< inorder.size();
     }
 };
 
