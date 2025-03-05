02e773a6eec3032337db131f6d27a1c97c8f9e00
AchyutPandey <121763772+AchyutPandey@users.noreply.github.com>
2025-03-06 00:18:27 +0530

    Time: 2 ms (81.24%), Space: 10.1 MB (46.94%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,5 +1,7 @@
+typedef long long ll;
 class Solution {
     bool issubstr(string a, string b){
+
         int s=a.size();
         int l=b.size();
         for(int i=0;i<=l-s;i++){
@@ -14,6 +16,47 @@ class Solution {
         }
         return false;
     }
+    
+    int rabinkarp(string &src, string &tar){
+        
+        int p=31;
+        int mod=1000000+7;
+        //int temp=1;
+        int srchash=0,tarhash=0;
+        int tlen=tar.size();
+        int slen=src.size();
+        if((slen<tlen)||slen==0||tlen==0)return -1;
+
+        for(int i=0;i<tlen;i++){
+            tarhash=((tarhash%mod)*p)%mod;
+            tarhash=(tarhash%mod+int(tar[i]-'a')+1)%mod;            
+        }
+       // cout<<"thash"<<tarhash<<endl;
+
+        int fact=1;
+        for(int i=0;i<tlen;i++){
+            srchash=((srchash%mod)*p)%mod;
+            srchash=(srchash+int(src[i]-'a')+1)%mod;
+            //if(i==0)continue;
+        
+            fact=((fact%mod)*p)%mod;
+        }
+        //cout<<"fact"<<fact<<endl;
+        for(int i=tlen;i<slen;i++){
+            if(srchash==tarhash){
+                return i-tlen;
+            }
+            ///cout<<"shash"<<srchash<<endl;
+            srchash=((srchash%mod)*p)%mod;
+            srchash+=(mod+int(src[i]-'a')+1-(int(src[i-tlen]-'a')+1)*fact)%mod;
+            srchash=(srchash+mod)%mod;
+        }
+        //cout<<srchash<<endl;
+        if(srchash==tarhash){
+            return slen-tlen;
+        }
+        return -1;
+    }
 public:
     int repeatedStringMatch(string a, string b) {
         int as=a.size();
@@ -23,13 +66,17 @@ public:
         for(int i=0;i<n;i++){
             t+=a;
         }
-        if(issubstr(b,t)){
+        if(rabinkarp(t,b)!=-1){
+            
             return n;
         }
         t=t+a;
-        if(issubstr(b,t)){
+        cout<<"no "<<n<<endl;
+        if(rabinkarp(t,b)!=-1){
+            
             return n+1;
         }
+        cout<<"no "<<n+1<<endl;
         return -1;
     }
 };
